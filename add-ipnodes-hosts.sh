c4a00867596d08af0f59a36236f7482d790f8695
atilacastro <f94496c@fiatauto.adfa.local>
2019-09-11 17:44:38 -0300

    First Commit - Add File

---
+++ add-ipnodes-hosts.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#name: add-ipnodes-hosts.sh
+
+#Variables
+HOSTFILE="/etc/hosts"
+
+sed -i 's/10.164.35.125 awsnode01/#/g' $HOSTFILE
+
+#Write ip nodes
+echo "#########################" >> $HOSTFILE
+echo "# Test AWS - Leonardo Almeida" >> $HOSTFILE
+echo "# " >> $HOSTFILE
+echo "$2                      $1" >> $HOSTFILE
+echo "$2                      $1" >> $HOSTFILE
+echo "$2                      $1" >> $HOSTFILE
+echo "$2                      $1" >> $HOSTFILE
+echo "$2                      $1" >> $HOSTFILE
+echo "$2                      $1" >> $HOSTFILE
+echo "$2                      $1" >> $HOSTFILE
+echo "$2                      $1" >> $HOSTFILE
+echo "#################################" >> $HOSTFILE
+
+#Just execute if need to change the hostname
+#Change hostname
+hostnamectl set-hostname $1
+
+#Restart Network interface
+/etc/init.d/network restart
+
+cat /etc/hosts
