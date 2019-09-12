5e95be3122fccbdcd92faffa9a860db7e86b9624
Átila Castro <f94496c@fiatauto.adfa.local>
2019-09-12 10:00:51 -0300

    Change main file

--- add-ipnodes-hosts.sh
+++ add-ipnodes-hosts.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 #name: add-ipnodes-hosts.sh
-# $1 hostname
-# $2 IP
 
 #Variables
 HOSTFILE="/etc/hosts"
@@ -11,9 +9,16 @@ sed -i 's/10.164.35.125 awsnode01/#/g' $HOSTFILE
 
 #Write ip nodes
 echo "#########################" >> $HOSTFILE
-echo "# Test AWS - Leonardo Almeida" >> $HOSTFILE
+echo "# Hosts nodes HPC" >> $HOSTFILE
 echo "# " >> $HOSTFILE
-echo "$2                      $1" >> $HOSTFILE
+echo "10.164.20.33              awsnode15" >> $HOSTFILE
+echo "10.164.20.21              awsnode16" >> $HOSTFILE
+echo "10.164.20.147             awsnode17" >> $HOSTFILE
+echo "10.164.21.26              awsnode18" >> $HOSTFILE
+echo "10.164.20.172             awsnode19" >> $HOSTFILE
+echo "10.164.20.116             awsnode20" >> $HOSTFILE
+echo "10.164.20.210             awsnode21" >> $HOSTFILE
+echo "10.164.21.63              awsnode22" >> $HOSTFILE
 echo "#################################" >> $HOSTFILE
 
 #Just execute if need to change the hostname
