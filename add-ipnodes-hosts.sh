721e4685401f4604933ed7bf1042fa9815136d35
Átila Castro <f94496c@fiatauto.adfa.local>
2019-09-13 14:52:24 -0300

    Change main file and add new file node-ip

--- add-ipnodes-hosts.sh
+++ add-ipnodes-hosts.sh
@@ -4,24 +4,17 @@
 
 #Variables
 HOSTFILE="/etc/hosts"
+NODEIP=$(cat /tmp/nodeip)
 
 sed -i 's/10.164.35.125/#/g' $HOSTFILE
 sed -i 's/awsnode01/#/g' $HOSTFILE
 
 #Write ip nodes
+for LINE in ${NODEIP[@]}; do
 echo "#########################" >> $HOSTFILE
-echo "# Hosts nodes HPC" >> $HOSTFILE
-echo "# " >> $HOSTFILE
-echo "10.164.20.33              awsnode15" >> $HOSTFILE
-echo "10.164.20.21              awsnode16" >> $HOSTFILE
-echo "10.164.20.147             awsnode17" >> $HOSTFILE
-echo "10.164.21.26              awsnode18" >> $HOSTFILE
-echo "10.164.20.172             awsnode19" >> $HOSTFILE
-echo "10.164.20.116             awsnode20" >> $HOSTFILE
-echo "10.164.20.210             awsnode21" >> $HOSTFILE
-echo "10.164.21.63              awsnode22" >> $HOSTFILE
+CONTAINERSTATUS=$(echo "$LINE" >> $HOSTFILE)
 echo "#################################" >> $HOSTFILE
-
+done
 #Just execute if need to change the hostname
 #Change hostname
 hostnamectl set-hostname $1
