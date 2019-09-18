2f29d18c6f7d2d4f06d2b1d5a5d365b20a0ae7b9
Átila Castro <atilacastro@gmail.com>
2019-09-18 16:44:10 +0000

    Change files in the server

--- files/add-ipnodes-hosts.sh
+++ files/add-ipnodes-hosts.sh
@@ -4,24 +4,21 @@
 
 #Variables
 HOSTFILE="/etc/hosts"
-NODEIP=$(cat /tmp/node-ip)
+NODEIP=$(cat /tmp/node-ip.txt)
 
-sed -i 's/10.164.35.125/#/g' $HOSTFILE
-sed -i 's/awsnode01/#/g' $HOSTFILE
+#sed -i 's/10.164.35.125/#/g' $HOSTFILE
+#sed -i 's/awsnode01/#/g' $HOSTFILE
 
 #Write ip nodes
 IFS=$'\n'       # make newlines the only separator
 for LINE in ${NODEIP[@]}; do
-        echo $LINE >> $HOSTFILE
+       sudo echo $LINE >> $HOSTFILE
 done
 #Just execute if need to change the hostname
 #Change hostname
-hostnamectl set-hostname $1
+#hostnamectl set-hostname $1
 
 #Restart Network interface
-/etc/init.d/network restart
+#/etc/init.d/network restart
 
-cat /etc/hosts
-
-rm -rf /tmp/add-ipnodes-hosts.sh
-rm -rf /tmp/node-ip
+#cat /etc/hosts
